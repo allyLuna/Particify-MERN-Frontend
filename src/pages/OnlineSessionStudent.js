18f8694d9aefe410854dc03b67fa9e455aa11bb8
Ally <alisa.luna27@gmail.com>
2023-01-08 13:14:06 +0800

    io url is netligy

--- src/pages/OnlineSessionStudent.js
+++ src/pages/OnlineSessionStudent.js
@@ -248,7 +248,7 @@ const OnlineSessionStudent = () => {
 //-------------SERVER CONNECTION---------------
     
    
-    const socket = io.connect(process.env.PORT);
+    const socket = io.connect("https://enchanting-madeleine-c3ff07.netlify.app");
     
     // kay useeffect dapat ung paghcnage ng leaderboards
     useEffect(() => {
