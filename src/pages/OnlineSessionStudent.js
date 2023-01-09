03a7c55026977ff55d454df7e2088266b002e2f4
Ally <alisa.luna27@gmail.com>
2023-01-09 16:10:45 +0800

    :80

--- src/pages/OnlineSessionStudent.js
+++ src/pages/OnlineSessionStudent.js
@@ -250,7 +250,7 @@ const OnlineSessionStudent = () => {
    
     //const socket = io.connect("https://particify-backend.adaptable.app:80");
     
-    const socket = io.connect('https://particify-backend.adaptable.app',
+    const socket = io.connect('https://particify-backend.adaptable.app:80',
         { //cors:{origin: "https://enchanting-madeleine-c3ff07.netlify.app"} ,
          transports: ['polling','websocket'],
          upgrade:false})
