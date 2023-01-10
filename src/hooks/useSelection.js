7aeec7c482424fb5a7ff08142b448ad0c2e7516d
Ally <alisa.luna27@gmail.com>
2023-01-10 21:22:16 +0800

    cors

--- src/hooks/useSelection.js
+++ src/hooks/useSelection.js
@@ -23,10 +23,8 @@ export const useSelection = () => {
         
             headers: {'Content-Type': 'application/json',
                       'Authorization': `Bearer ${student.token}`,
-                      //'Access-Control-Allow-Origin' : 'https://particify.netlify.app',
-                      //'Access-Control-Allow-Credentials' : true},
-        },
-                      mode: "no-cors",
+                      'Access-Control-Allow-Origin' : 'https://particify.netlify.app',
+                      'Access-Control-Allow-Credentials' : true},
             body: JSON.stringify(selectionDts)
         })
 
