b11cdcd6d2f0cdee22691abdffa5540d40945a22
Ally <alisa.luna27@gmail.com>
2023-01-11 22:57:17 +0800

    ajdf

--- src/pages/OnlineSessionStudent.js
+++ src/pages/OnlineSessionStudent.js
@@ -7,10 +7,12 @@ import io from "socket.io-client";
 import { useEffect } from "react";
 import { useFacultySetting } from '../hooks/useFacultySetting'
 import { useSession } from "../hooks/useSession";
-
-const url = 'https://mernsample-tool.adaptable.app'
+const url = "https://particify-backend.adaptable.app";
 
 const OnlineSessionStudent = () => {
+
+    
+
     var [Score, setScore ] = useState(2000)
     var [parAsk, setAsk ] = useState(0)
     var [parRec, setRec ] = useState(0)
@@ -20,7 +22,7 @@ const OnlineSessionStudent = () => {
     const {student} = useAuthContext()
     const {session} = useFacultySetting()
     const {selection, error} = useSelection();
-    const [currScore, setCurrentScore] = useState()
+    const [currScore, setCurrentScore] = useState();
 
     // for dialog titles
     var[dlgread, setDlg] = useState(0)
@@ -42,72 +44,71 @@ const OnlineSessionStudent = () => {
     const [sresults3, setsResults3] = useState([]);
   //  var theScores = [];
 
-
 //-----------------ACTION BUTTONS-------------------------------
 
-    //Ask
-    const btnAsk = async (e) => {
+   //Ask
+   const btnAsk = async (e) => {
         
-        e.preventDefault()
-        console.log(e.timeStamp, e.target.value);
-        // input for selection
-        await selection(student.username, e.timeStamp , e.target.value);
-       
-       
-       // setShowTaskDlg(true);
-        setScore(Score = Score + 100);
-        setAsk(parAsk+1);
-        //setdlgTitle(e.target.value)
-        setidSelected("Askaquestion")
-        setDlg(dlgread = 1);
-        fetchFirst();
-        updateScore();
-        titleDlg(); updateFreq();
-        socket.emit("send_message", room);
-       // console.log({room})
-        
-    }
-        
-    //Recite  
-    const btnRecite = async (e) =>  {
-        e.preventDefault()
-        console.log(e.timeStamp);
-         // input for selection
-        await selection(student.username,e.timeStamp , e.target.value)
-        
-        setDlg(dlgread = 2);
-        //setShowTaskDlg(true);
-        setdlgTitle(e.target.value)
-        setScore(Score = Score + 500);
-        setRec(parRec+1);
-        setidSelected("Recite");
-        fetchFirst();
-        updateScore();
-        titleDlg();updateFreq();
-       socket.emit("send_message", room);
-       
-        
-    }
+    e.preventDefault()
+    console.log(e.timeStamp, e.target.value);
+    // input for selection
+    await selection(student.username, e.timeStamp , e.target.value);
+   
+   
+   // setShowTaskDlg(true);
+    setScore(Score = Score + 100);
+    setAsk(parAsk+1);
+    //setdlgTitle(e.target.value)
+    setidSelected("Askaquestion")
+    setDlg(dlgread = 1);
+    fetchFirst();
+    updateScore();
+    titleDlg(); updateFreq();
+    socket.emit("send_message", room);
+   // console.log({room})
+    
+}
+    
+//Recite  
+const btnRecite = async (e) =>  {
+    e.preventDefault()
+    console.log(e.timeStamp);
+     // input for selection
+    await selection(student.username,e.timeStamp , e.target.value)
+    
+    setDlg(dlgread = 2);
+    //setShowTaskDlg(true);
+    setdlgTitle(e.target.value)
+    setScore(Score = Score + 500);
+    setRec(parRec+1);
+    setidSelected("Recite");
+    fetchFirst();
+    updateScore();
+    titleDlg();updateFreq();
+   socket.emit("send_message", room);
+   
+    
+}
 
-    //Give Idea
-    const btnGive = async (e) =>  {
-        e.preventDefault()
-        console.log(e.timeStamp);
-         // input for selection
-        await selection(student.username,e.timeStamp , e.target.value)
-       
-        setDlg(dlgread = 3);
-        //setShowTaskDlg(true);
-        setdlgTitle(e.target.value)
-        setScore(Score = Score + 200);
-        setGive(parGive+1);
-        fetchFirst();
-        updateScore();
-        titleDlg(); updateFreq();
-        setidSelected("Giveoutidea")
-       socket.emit("send_message", room);
-       
-    };
+//Give Idea
+const btnGive = async (e) =>  {
+    e.preventDefault()
+    console.log(e.timeStamp);
+     // input for selection
+    await selection(student.username,e.timeStamp , e.target.value)
+   
+    setDlg(dlgread = 3);
+    //setShowTaskDlg(true);
+    setdlgTitle(e.target.value)
+    setScore(Score = Score + 200);
+    setGive(parGive+1);
+    fetchFirst();
+    updateScore();
+    titleDlg(); updateFreq();
+    setidSelected("Giveoutidea")
+   socket.emit("send_message", room);
+   
+};
 
     
 //---------------------------END----------------------------
@@ -143,7 +144,14 @@ const OnlineSessionStudent = () => {
 
 //-----FOR STUDENT SELECTION--------
     const fetchFirst = async (e) => {
-        const response = await fetch(url + '/api/students/selectedFirst')
+        const response = await fetch(url + '/api/students/selectedFirst', {
+            method: 'GET',
+            headers: {'Content-Type': 'application/json',
+                    //  'Authorization': `Bearer ${student.token}`},
+                    'Access-Control-Allow-Origin' : 'https://particify.netlify.app', 
+                      'Access-Control-Allow-Credentials' : true
+            
+    }})
         const data = await response.json()
          setSelected(data.student_uname)
         //setidStud(data.id)
@@ -156,38 +164,39 @@ const OnlineSessionStudent = () => {
 
 //-----FOR UPDATING SCORE, FREQUENCY IN DB: DELETING SELECTION, GETTING LEADERBOARD--------
     
-    //score update
-    const updateScore = async (e) => {
+   //score update
+   const updateScore = async (e) => {
         
-        const response = await fetch(url + '/api/students/updateScore/' + selected, {
-            method: 'PATCH',
-            headers: {'Content-Type': 'application/json',
-                      'Authorization': `Bearer ${student.token}`},
-            body:   JSON.stringify({score:Score})
-            
-        })
+    const response = await fetch(url + '/api/students/updateScore/' + selected, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${student.token}`},
+        body:   JSON.stringify({score:Score})
         
-        const data = await response.json()
-         
-         return data;
-    };
+    })
+    
+    const data = await response.json()
+     
+     return data;
+};
 
-    //frequency update
-    const updateFreq = async (e) => {
+//frequency update
+const updateFreq = async (e) => {
+    
+    const response = await fetch(url + '/api/students/updateParticipation/' + student.username, {
+        method: 'PATCH',
+        headers: {'Content-Type': 'application/json',
+                  'Authorization': `Bearer ${student.token}`},
+        body:   JSON.stringify({participationAsk:parAsk, participationRec:parRec,participationGive:parGive})
         
-        const response = await fetch(url + '/api/students/updateParticipation/' + student.username, {
-            method: 'PATCH',
-            headers: {'Content-Type': 'application/json',
-                      'Authorization': `Bearer ${student.token}`},
-            body:   JSON.stringify({participationAsk:parAsk, participationRec:parRec,participationGive:parGive})
-            
-        })
-        
-        const data = await response.json()
-        
-         return data;
-    };
+    })
+    
+    const data = await response.json()
+    
+     return data;
+};
 
+    //for deletion 
     //for deletion 
     const deleteSelection = async (e) => {
         
@@ -207,7 +216,14 @@ const OnlineSessionStudent = () => {
     const getResults = async (e) => {
         
        
-            const response = await fetch(url + '/api/students/getResults')
+            const response = await fetch(url + '/api/students/getResults',{
+                method: 'GET',
+                headers: {'Content-Type': 'application/json',
+                        //  'Authorization': `Bearer ${student.token}`},
+                        'Access-Control-Allow-Origin' : 'https://particify.netlify.app',
+                          'Access-Control-Allow-Credentials' : true
+                
+        }})
             const data = await response.json()
           
           setuResults(data[0].username)
@@ -222,7 +238,8 @@ const OnlineSessionStudent = () => {
     };
 
     //get current score
-    const getStudentScore = async (e) => {
+     //get current score
+     const getStudentScore = async (e) => {
         
         const response = await fetch(url + '/api/students/getScore/' + student.username)
         const data = await response.json()
@@ -235,7 +252,15 @@ const OnlineSessionStudent = () => {
 
     const theReward = async (e) => {
         
-    const response = await fetch(url + '/api/faculty/getReward/' + session.class_Code)
+    const response = await fetch(url + '/api/faculty/getReward/' + session.class_Code,{
+        method: 'GET',
+        headers: {'Content-Type': 'application/json',
+                //  'Authorization': `Bearer ${student.token}`},
+                'Access-Control-Allow-Origin' : 'https://particify.netlify.app',
+                  'Access-Control-Allow-Credentials' : true
+        
+}}
+    )
     const data = await response.json()
     setRewards(data[0].class_Reward)
     console.log(data[0].class_Reward)
@@ -252,8 +277,8 @@ const OnlineSessionStudent = () => {
    
     //const socket = io.connect("https://particify-backend.adaptable.app:80");
     
-    const socket = io.connect('https://mernsample-tool.adaptable.app/',
-        { //cors:{origin: "https://enchanting-madeleine-c3ff07.netlify.app"} ,
+    const socket = io.connect('https://particify-backend.adaptable.app',
+        { //cors:{ origin: "https://enchanting-madeleine-c3ff07.netlify.app"} ,
          transports: ['websocket','polling'],
          upgrade:false})
     // kay useeffect dapat ung paghcnage ng leaderboards
@@ -265,7 +290,7 @@ const OnlineSessionStudent = () => {
            setShowTaskDlg(true);	
            
     });
-    }, [])
+    }, [socket])
 
     
 //-------------END------------------------------
@@ -274,7 +299,7 @@ const OnlineSessionStudent = () => {
     return(
         <><>
         
-        <div class="body dark">
+        <div className="body dark">
             <div className="center2 recite">
             <h2 className="score" id="currentScore">{student.username}: {currScore}</h2>
             <br /><br /><br />
