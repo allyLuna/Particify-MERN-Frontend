b1e6a36f6cbbf0fc86c6cbea41378bb2f30224b0
Ally <alisa.luna27@gmail.com>
2023-01-11 20:30:57 +0800

    score

--- src/pages/OnlineSessionStudent.js
+++ src/pages/OnlineSessionStudent.js
@@ -21,8 +21,8 @@ const OnlineSessionStudent = () => {
     const {student} = useAuthContext()
     const {session} = useFacultySetting()
     const {selection, error} = useSelection();
-    const [currScore, setCurrentScore] = useState()
-
+    const [currScore, setCurrentScore] = useState(0)
+    
     // for dialog titles
     var[dlgread, setDlg] = useState(0)
     
@@ -254,6 +254,7 @@ const OnlineSessionStudent = () => {
             
     }})
         const data  = await response.json()
+
         setCurrentScore(currScore = data[0].score)
         setAsk(data.participationAsk);
         setRec(data.participationRec);
